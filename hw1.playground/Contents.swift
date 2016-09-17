370a1aed24cb9a3ea47e340a5b735385a80666d8
Jaewon Lee <dlwodnjs42@berkeley.edu>
2016-09-17 10:40:16 -0700

    finished hw

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,33 +22,40 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: They are not considered the same time because it has an optional.
+//: an optional illustrates that the String or whatever type can either be a String
+//: or a nil.
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
-        let reversedWords = words.map() {String($0.characters.reversed())}
+    class func arePalindromes(_ words: [String]) -> Bool {
+        let reversedWords = words.map() {($0.characters.reversed())}
         let numElements = words.count
         
         for i in 0 ..< numElements {
-            if words[i] != reversedWords[i] {
+            if words[i] != String(reversedWords[i]) {
                 return false
             }
+            
         }
+        return true
     }
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: $0 is a string array so you cant cast it as a string
+//: but since when you recurse through the reversedWords, you need to let the compire know what type it is, we can just cast it as a string
+//: forgot to return true if nothing happens and also the fact that this arePalindromes are called not by a instance but as a class function of the class Words.
+    
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    func isAnagram() -> Bool? {
+        var countLetters = [Character : Int]() //Line X
+        let lenA = self.wordA.characters.count
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -56,7 +63,6 @@ class Words {
         
         var arrA = Array(self.wordA.characters)
         var arrB = Array(self.wordB.characters)
-        
         for i in 0...lenA-1 {
             let letter = arrA[i]
             if let val = countLetters[letter] { //Line Y
@@ -75,7 +81,7 @@ class Words {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
@@ -88,9 +94,9 @@ class Words {
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
-
-//: [EXPLAIN YOUR ANSWER HERE]
-    
+//: the code below illustrates that they made a word object creating an instance of Words which means that its under the class Words not a class by itself so must erase Words.
+//: let -> var because let will not be able to changed while var will and we are increasing countLetters.
+//: Bool should be an optional because we returned nil at the end
     
 }
 
